b7859316309ec37af0cca832a088186a918134ff
jogotempo448 <71750123+jogotempo@users.noreply.github.com>
2025-08-10 02:54:29 +0700

    Add .next/static/chunks/src_app_page_tsx_3d3201a7._.js 1754769284941

---
+++ .next/static/chunks/src_app_page_tsx_3d3201a7._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_cbf79b29._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_f4662847._.js",
+  "static/chunks/node_modules_recharts_es6_cc756ff7._.js",
+  "static/chunks/node_modules_bf7d7fc1._.js"
+],
+    source: "dynamic"
+});
